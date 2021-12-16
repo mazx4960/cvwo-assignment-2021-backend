8d0ec3adadaecbe8998f21565690fa3dd13428c9
Desmond <desmondtan4561@gmail.com>
2021-12-16 14:42:16 +0800

    :wrench: Config

--- database/connection.go
+++ database/connection.go
@@ -12,7 +12,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dbinfo := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Singapore", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	dbinfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Singapore", os.Getenv("DB_ADDR"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 	connection, err := gorm.Open(postgres.Open(dbinfo), &gorm.Config{})
 
 	if err != nil {
